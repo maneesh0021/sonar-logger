c05e4a9667ba74815284a7bdea1f3aa9d0b702f5
maneeshbachu <maneesh.bachu@zemosolabs.com>
2025-06-27 18:13:22 +0530

    updated the Homecontroller.java

--- src/main/java/com/example/controller/HomeController.java
+++ src/main/java/com/example/controller/HomeController.java
@@ -1,13 +1,15 @@
 package com.example.controller;
 
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Slf4j
 @Controller
 public class HomeController {
+
+    private static final Logger log = LoggerFactory.getLogger(HomeController.class);
 
     @GetMapping("/home")
     public String home(Model model) {
